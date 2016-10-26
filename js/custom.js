43dbcd09b9ec1ce044c02be6ad88e2b23ecb14ff
Deepak <er.d.ahuja@gmail.com>
2016-10-26 15:31:39 +0530

    second

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/*
+
+    function data(str){
+        console.log(str);
+
+               $.ajax( {
+                  url:str,
+                   datatype:'text/html';
+                  success:function(data) {
+                     $('#main').html(data);
+                  }
+               });
+          
+    }
+*/
